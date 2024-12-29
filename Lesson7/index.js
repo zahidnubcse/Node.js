6bad6906e13cb39ecc8ca12390082d5ddeb91e4e
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-12-29 15:12:41 +0600

    external module

---
+++ Lesson7/index.js
@@ -0,0 +1,11 @@
+// external module
+//fuirts
+
+const randomFruitsName = require('random-fruits-name');
+
+console.log(randomFruitsName('en'));
+
+//movie name
+
+const movieInfo = require('movie-info');
+movieInfo('Gladiator').then(console.log)
